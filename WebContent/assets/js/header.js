add1139bc4cdd753a784f535d37daaa2c2e39312
Mr.lu <sekift@163.com>
2018-04-09 15:03:54 +0800

    Initial commit

---
+++ WebContent/assets/js/header.js
@@ -0,0 +1,16 @@
+!function () {
+    var listHeader = [
+        '<ul class="am-nav am-nav-pills am-nav-justify">',
+        '<li class="am-u-lg-2 am-u-sm-12"><a href=""><img src="assets/images/logo2.jpg" alt="" style="width:90%;height:90%;"/></a></li>',
+        '<li></li>',
+        '<li class=""><a href="http://www.bubbt.com">首页</a></li>',
+        '<li><a href="http://www.bubbt.com/mitiao/">密条</a></li>',
+        '<li><a href="#">案读</a></li>',
+        '<li><a href="#">图读</a></li>',
+        '<li><a href="#">句读</a></li>',
+        '<li><a href="#">博客</a></li>',
+        //'<li><a href="html/about.html">关于</a></li>',
+        '</ul>'
+    ].join("")
+    document.write(listHeader)
+}();
---
+++ WebContent/assets/js/header_m.js
@@ -0,0 +1,30 @@
+!function () {
+    var listHeader = [
+        '<div class="am-u-sm-2">',
+        '<div class="menu-bars">',
+        '<a href="#doc-oc-demo1" data-am-offcanvas="{effect: \'push\'}"><i class="am-menu-toggle-icon am-icon-bars"></i></a>',
+        '<nav data-am-widget="menu" class="am-menu  am-menu-offcanvas1" data-am-menu-offcanvas >',
+        '<a href="javascript: void(0)" class="am-menu-toggle"></a>',
+        '<div class="am-offcanvas" >',
+        '<div class="am-offcanvas-bar">',
+        '<ul class="am-menu-nav am-avg-sm-1">',
+        '<li><a href="http://www.bubbt.com" class="" >首页</a></li>',
+        '<li><a href="#">案读</a></li>',
+        '<li><a href="#">图读</a></li>',
+        '<li><a href="#">句读</a></li>',
+        '<li><a href="#">博客</a></li>',
+        '</ul>',
+        '</div>',
+        '</div>',
+        '</nav>',
+        '</div>',
+        '</div>',
+        '<div class="am-u-sm-5 am-u-end">',
+        '<div class="m-logo">',
+        '<a href=""><img src="assets/images/logo2.jpg" alt="" style="width:40%;height:40%;"></a>',
+        '</div>',
+        '</div>'
+
+    ].join("")
+    document.write(listHeader)
+}();
